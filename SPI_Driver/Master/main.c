cca7a0d48e40e12a06675d2414221c0b2767875c
Khalid Beneddine <khaledbeneddine@gmail.com>
2020-06-15 23:46:02 +0200

    SPI Matser Update

--- SPI_Driver/Master/main.c
+++ SPI_Driver/Master/main.c
@@ -9,12 +9,13 @@
 #include <xc.h>
 #include <stdint.h>
 #include"Config.h"
-#define _XTAL_FREQ 4000000
+#define _XTAL_FREQ 4000000 // Fose
 
 // Define Button
 #define UP RB0
 #define DOWN RB1
 #define SEND RB2
+
 //Function declaration
 void SPI_init ();
 void SPI_Write(uint8_t);
@@ -44,19 +45,24 @@ void main(void) {
 }
 
 void SPI_init() {
-    //Master mode with prescl4
+
+//Master mode with prescl4
 SSPM0 = 0;
 SSPM1 = 0;
 SSPM2 = 0;
 SSPM3 = 0;
-    //Syn mode
+
+//Syn mode
 SSPEN = 1;
-    //Config CLK polarity & phase
+
+//Config CLK polarity & phase
 CKP = 0;
 CKE = 0;
-    //Config the input data sampled
+
+//Config the input data sampled
 SMP = 0;
-    //Config I/O Pins
+
+//Config I/O Pins
 TRISC5 = 0; //SDO
 TRISC4 = 1; //SDI
 TRISC3 = 0; //SCK
